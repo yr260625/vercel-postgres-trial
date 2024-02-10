97256118b9969c5d35661a99c63edb079263e8a4
yr260625 <yr260625.learning@gmail.com>
2024-02-10 12:37:48 +0900

    [FIX] config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   apiPrefix: process.env.NEXT_PUBLIC_API_PREFIX ?? "http://",
-  apiHost: process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000",
+  apiHost: process.env.VERCEL_URL ?? "localhost:3000",
 };
