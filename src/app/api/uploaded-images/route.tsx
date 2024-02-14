e4372722c43264dfb2ee5ad480ae2f4b676e58ea
yr260625 <yr260625.learning@gmail.com>
2024-02-14 22:56:56 +0900

    [FIX] test

--- src/app/api/uploaded-images/route.tsx
+++ src/app/api/uploaded-images/route.tsx
@@ -1,8 +1,8 @@
 "use server";
-
 import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const data = await sql`select * from uploaded_images`;
-  return Response.json(data.rows);
+  return NextResponse.json(data.rows);
 }
