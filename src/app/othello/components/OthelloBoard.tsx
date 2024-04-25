190cf67453c27deb69abc5459f2ee272bebb3dab
yr260625 <yr260625.learning@gmail.com>
2024-04-25 23:49:53 +0900

    [FIX] fine tuning

--- src/app/othello/components/OthelloBoard.tsx
+++ src/app/othello/components/OthelloBoard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './OthelloBoard.module.css';
 import { OthelloUsecases } from '@/app/othello/features/usecases';
 import { GAME_STATUS, GAME_TURN, INIT_BOARD, Point } from '@/app/othello/common';
