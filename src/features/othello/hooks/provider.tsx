846aef835ed7887ed85f3b59b2bcb8084c811410
yr260625 <yr260625.learning@gmail.com>
2024-06-22 15:22:32 +0900

    [FIX] provider

--- src/features/othello/hooks/provider.tsx
+++ src/features/othello/hooks/provider.tsx
@@ -5,13 +5,18 @@ type ProviderProps = {
   children: React.ReactNode;
 };
 
-type OthelloContextType = {
-  othelloState: OthelloState;
-  setOthelloState: (newState: Partial<OthelloState>) => void;
-};
+// providerで共有する値の型
+type StateContext = OthelloState;
+type StateChangerContext = (newState: Partial<OthelloState>) => void;
+const OthelloStateContext = createContext<StateContext>({} as StateContext);
+const OthelloStateChangerContext = createContext<StateChangerContext>(
+  {} as StateChangerContext
+);
 
-const OthelloContext = createContext<OthelloContextType>({} as OthelloContextType);
-export const OthelloProvider = ({ children }: ProviderProps) => {
+/**
+ * 初期状態を取得するカスタムフック
+ */
+const useInitOthelloState = (): { othelloState: OthelloState; setOthelloState: any } => {
   const [othelloState, setOthelloState] = useState<OthelloState>(OTHELLO_INIT_STATE);
 
   // setOthelloStateをuseCallbackでメモ化する
@@ -22,15 +27,29 @@ export const OthelloProvider = ({ children }: ProviderProps) => {
     }));
   }, []);
 
-  // Providerで各コンポーネントに配る
-  const value: OthelloContextType = {
-    othelloState,
-    setOthelloState: memoizedSetOthelloState,
-  };
-  return <OthelloContext.Provider value={value}>{children}</OthelloContext.Provider>;
+  return { othelloState, setOthelloState: memoizedSetOthelloState };
 };
 
-// コンポーネント間で使いまわすコンテキスト
-export const useOthelloState = (): OthelloContextType => {
-  return useContext(OthelloContext);
+/**
+ * プロバイダで各種コンポーネントに共有
+ *
+ */
+export const OthelloProvider = ({ children }: ProviderProps): any => {
+  const { othelloState, setOthelloState } = useInitOthelloState();
+  return (
+    <OthelloStateContext.Provider value={othelloState}>
+      <OthelloStateChangerContext.Provider value={setOthelloState}>
+        {children}
+      </OthelloStateChangerContext.Provider>
+    </OthelloStateContext.Provider>
+  );
+};
+
+/**
+ * コンポーネント間で使いまわす状態および状態更新関数
+ */
+export const useOthelloState = () => {
+  const othelloState = useContext(OthelloStateContext);
+  const setOthelloState = useContext(OthelloStateChangerContext);
+  return { othelloState, setOthelloState };
 };
